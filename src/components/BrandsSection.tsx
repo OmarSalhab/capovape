0236b787da197b6eb88569b7f7518b0c24c9b461
OmarSalhab <omarsalhb24@gmail.com>
2025-09-23 18:15:55 +0300

    feat: replace Image component with img tag in BrandsSection for improved performance

--- src/components/BrandsSection.tsx
+++ src/components/BrandsSection.tsx
@@ -55,11 +55,9 @@ export default function BrandsSection() {
 									}}
 								>
 									<div className="rounded-full h-full w-full bg-black/80 grid place-items-center border border-[#3a2f15]">
-										<Image
+										<img
 											src={brand.image}
 											alt={label}
-											width={160}
-											height={80}
 											className="w-20 h-20 md:w-24 md:h-24 object-contain opacity-90 group-hover:opacity-100 transition mix-blend-screen"
 										/>
 									</div>
